ba365e72eaef8f2b3126ac45125231ee6407c06f
isabellavieira57 <isabellavieira57@gmail.com>
2018-10-15 12:43:47 -0400

    Adapting the query to get only Docker SO posts

--- get_SOposts.py
+++ get_SOposts.py
@@ -1,16 +1,28 @@
 from google.cloud import bigquery
 import os
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 
+# sudo pip install --upgrade google-cloud-bigquery --ignore-installed six
+# Create Google Credentials: create the account key on google cloud: https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries#bigquery_simple_app_query-python
+# export GOOGLE_APPLICATION_CREDENTIALS="<FILE>.json"
+
+# Add the json file with the credentials here
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 
 def get_stackoverflow_posts ():
 	client = bigquery.Client()
 
+	# get_SOPosts = """
+	# SELECT p.*, pt.Type, c.Text FROM `sotorrent-org.2018_09_23.Posts` as p, 
+	# `sotorrent-org.2018_09_23.PostType` as pt, `sotorrent-org.2018_09_23.Comments` as c 
+	# WHERE p.PostTypeId = pt.Id and p.Id = c.PostId limit 10;
+	# """
+
 	get_SOPosts = """
-	SELECT p.*, pt.Type, c.Text FROM `sotorrent-org.2018_09_23.Posts` as p, 
-	`sotorrent-org.2018_09_23.PostType` as pt, `sotorrent-org.2018_09_23.Comments` as c 
-	WHERE p.PostTypeId = pt.Id and p.Id = c.PostId limit 10;
+	SELECT DISTINCT p.*, pt.Type, c.Text, prgh.RepoName FROM `sotorrent-org.2018_09_23.Posts` as p, 
+	`sotorrent-org.2018_09_23.PostType` as pt, `sotorrent-org.2018_09_23.Comments` as c,
+	`sotorrent-org.2018_09_23.PostReferenceGH` as prgh
+	WHERE p.PostTypeId = pt.Id and p.Id = c.PostId and prgh.RepoName LIKE 'docker/docker.github.io';
 	"""
 
 	query_job = client.query(get_SOPosts)
@@ -18,9 +30,10 @@ def get_stackoverflow_posts ():
 
 	for row in results:
 		print ">>> ID: ", row.Id
-		print ">>> Title: ", row.Title
-		print ">>> Body: ", row.Body
-		print ">>> Comments: ", row.Text
+		print ">>> Title: ", row.Title.encode("utf-8")
+		print ">>> Body: ", row.Body.encode("utf-8")
+		print ">>> Comments: ", row.Text.encode("utf-8"):
+		print ">>> RepoName: ", row.RepoName
 		print "\n\n"
 	
 def get_stackoverflow_posts_over_time ():
@@ -34,8 +47,7 @@ def get_stackoverflow_posts_over_time ():
 		ph.PostId = tv.PostHistoryId and
 		ph.PostId = pv.PostHistoryId and
 		ph.PostId = pbv.PostHistoryId and
-		pbv.PostBlockTypeId = pbt.Id
-		limit 10;
+		pbv.PostBlockTypeId = pbt.Id;
 	"""
 
 	query_job = client.query(get_SOPostsOverTime)
@@ -49,4 +61,4 @@ def get_stackoverflow_posts_over_time ():
 
 if __name__ == '__main__':
     get_stackoverflow_posts()
-    get_stackoverflow_posts_over_time()
+   #get_stackoverflow_posts_over_time()
