e7cfb4eb2f107de43392c18764fe6c51bd94d17b
chupy35 <chupy35@gmail.com>
2018-10-17 19:55:38 -0400

    in order function

--- stackoverbug.py
+++ stackoverbug.py
@@ -1,18 +1,20 @@
 #import get_SOposts as stackoverflow
-#import simi as similarity
-import get_GHissues as github
+import simi as similarity
+import gh_get_issues as github
+#soPosts = stackoverflow.get_stackoverflow_posts()
 
 gitissues=github.get_gh_issues()
-
-
-soPosts = stackoverflow.get_stackoverflow_posts()
-
-
+readed = similarity.read_file(gitissues)
+tokenized = similarity.tokenize(readed)
+stemdicted = similarity.stemdicreat(tokenized)
+corpused = similarity.map_word_to_id(stemdicted)
+tfidfed = similarity.tfidf(corpused)
+simed = similarity.simmes(corpused, tfidfed, stemdicted)
 # For each stack overflow post, we compare to all bug reports and to the similarity
-for post in soPosts:
-	for bug in gitissues['items']:
-            print(bug['created_at'])
-            print(bug['comments_url'])
-            print(bug['title'])
-            print(bug['body'])
-
+#for post in soPosts:
+#	for bug in gitissues['items']:
+#            print(bug['created_at'])
+#            print(bug['comments_url'])
+#            print(bug['title'])
+#            print(bug['body'])
+#
