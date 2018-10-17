5c99533bbf0ec6a7f4b4be1d312f51daa5daac91
chupy35 <chupy35@gmail.com>
2018-10-17 19:25:18 -0400

    List of lists

--- gh_get_issues.py
+++ gh_get_issues.py
@@ -1,7 +1,5 @@
 import json
 import requests
-from pprint import pprint
-import os
 api_token = '6b29e7c228306634d4617dab25ad6788b5adecb0'
 api_url_base = 'https://api.github.com/'
 projects = ["docker/docker.github.io", "rails/rails", "googleapis/google-api-python-client", "prestodb/presto", "square/okhttp", "netty/netty", "elastic/elasticsearch-hadoop", "elastic/elasticsearch", "spring-projects/spring-boot", "tensorflow/tensorflow", "facebook/react", "angular/angular.js"]
@@ -16,6 +14,8 @@ headers = {'Content-Type': 'application/json',
            'Authorization': 'Bearer {0}'.format(api_token)}
 #---------------------------------------------------------
 def get_gh_issues(projects):
+    pissues = []
+    issues = []
     for p in projects:
         r = requests.get('https://api.github.com/search/issues?q=label:'+proj_lab[p]+'bug+state:closed+repo:'+p)
         results=r.text
@@ -28,7 +28,10 @@ def get_gh_issues(projects):
 
            for y in comments:
                commentsa = y['body'] + "\n"
-    return x['title'] + "\n" + x['number'] + "\n" + x['body'] + "\n" + commentsa
+        issues[x] = x['number'] + "\n" + x['title'] + "\n" + x['body'] + "\n" + commentsa
+    pissues[p] = issues
+    return pissues
+
 #           print("CREATED IN ---->   ", x['created_at'])
 #           print("CLOSED IN ---->   ", x['closed_at'])
 #           print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
