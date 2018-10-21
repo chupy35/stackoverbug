938c1b8a3454d7f29c7797563e2859b083c8bc75
chupy35 <chupy35@gmail.com>
2018-10-21 15:28:42 -0400

    manual minning

--- get_SOposts.py
+++ get_SOposts.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-import os
+import io
 
 """
 How to run:
@@ -14,66 +14,64 @@ How to run:
 
 # Explicitly use service account credentials by specifying the private
 # key file. All clients in google-cloud-python have this helper.
-client = bigquery.Client.from_service_account_json('/Users/isabellavieira/Downloads/chupytestcom-8a09729c6510.json')
+#clx00-\x7fient = bigquery.Client.from_service_account_json('/home/chupy35/polymtl/dataminning/assigment/test2/stackoverbug/chupytestcom-dfe002fa9cf6.json')
 
-tags = ["docker"]
-#, "ruby-on-rails", "netty", "elasticsearch","spring-boot",  "tensorflow", "reactjs", "angularjs"] 
+tags = "angular"
+#, "ruby-on-rails", "netty", "elasticsearch","spring-boot",  "tensorflow", "reactjs", "angularjs"]
 
-def get_stackoverflow_posts ():
-	client = bigquery.Client()
-	allResults = []
+client = bigquery.Client()
+allResults = []
+tmpResult = []
+
+i = 0
+for tag in tags:
 	tmpResult = []
-	
-	for tag in tags: 		
-		tmpResult = []
 
-		get_SOPosts = "SELECT p.Id, p.ParentId, p.Title, p.Body, p.CreationDate, p.Tags FROM `sotorrent-org.2018_09_23.Posts` as p  WHERE p.Tags LIKE '<"+ tag +">' AND p.ParentId IS NULL ORDER BY p.Id ASC;"
-		query_job = client.query(get_SOPosts)
-		results = query_job.result()
-
-		for row in results:
-			complete_post = ""
-			complete_post = str(row.Id) + "\n" + str(row.Title.encode("utf-8")) + "\n" + str(row.Body.encode("utf-8"))
-
-			get_comments = "SELECT p.Id, p.ParentId, p.Title, p.Body, p.Tags FROM `sotorrent-org.2018_09_23.Posts` as p WHERE p.parentId = "+ str(row.Id) +" ORDER BY p.Id ASC;"
-			query_job = client.query(get_comments)
-			results_comments = query_job.result()
-
-			for comment in results_comments:
-				complete_post = complete_post + "\n" + str(comment.Body.encode("utf-8"))
-			
-			# print ">>> Complete post: ", complete_post
-			# print "**********************************************************"
-			# print "\n\n"
-			tmpResult.append(complete_post)
-			print "Post id: ", row.Id                                              
-			print "Parent id: ", row.ParentId                                      
-			print "TAGS: ", tag                                                    
-			print "Creation Date: ", row.CreationDate                              
-			print "*******************************************************\n"
-		allResults.append(tmpResult)
-	return allResults
-
-	
-def get_stackoverflow_posts_over_time ():
-	client = bigquery.Client()
-
-	get_SOPostsOverTime = """
-		SELECT ph.*, pht.Type, tv.Title, pbv.Content FROM `sotorrent-org.2018_09_23.PostHistory` as ph, `sotorrent-org.2018_09_23.PostHistoryType` as pht, 
-		`sotorrent-org.2018_09_23.TitleVersion` as tv, `sotorrent-org.2018_09_23.PostVersion` as pv, 
-		`sotorrent-org.2018_09_23.PostBlockVersion` as pbv, `sotorrent-org.2018_09_23.PostBlockType` as pbt
-		WHERE ph.PostHistoryTypeId = pht.Id and 
-		ph.PostId = tv.PostHistoryId and
-		ph.PostId = pv.PostHistoryId and
-		ph.PostId = pbv.PostHistoryId and
-		pbv.PostBlockTypeId = pbt.Id;
-	"""
-
-	query_job = client.query(get_SOPostsOverTime)
+	get_SOPosts = "SELECT p.Id, p.ParentId, p.Title, p.Body, p.CreationDate, p.Tags FROM `sotorrent-org.2018_09_23.Posts` as p  WHERE p.Tags LIKE '<"+ tag +">' AND p.ParentId IS NULL ORDER BY p.Id ASC;"
+	query_job = client.query(get_SOPosts)
 	results = query_job.result()
 
-	return results
+for row in results:
 
-if __name__ == '__main__':
-	allResults = get_stackoverflow_posts()
-   	#get_stackoverflow_posts_over_time()
+    complete_post = ""
+    complete_post = str(row.Id) + "\n" + row.Title.encode("utf-8") + "\n" + row.Body.encode("utf-8")
+    i = i + 1
+    get_comments = "SELECT p.Id, p.ParentId, p.Title, p.Body, p.Tags FROM `sotorrent-org.2018_09_23.Posts` as p WHERE p.parentId = "+ str(row.Id) +" ORDER BY p.Id ASC;"
+    query_job = client.query(get_comments)
+    results_comments = query_job.result()
+
+    for comment in results_comments:
+        complete_post = complete_post + "\n" + comment.Body.encode("utf-8")
+    f = open(tags + "_" + str(row.Id) + '.txt', 'w')
+    f.write(complete_post)
+    #print "Post id: ", row.Id
+    #print "Parent id: ", row.ParentId
+    #print "TAGS: ", tag
+    #print "Creation Date: ", row.CreationDate
+    #print "*******************************************************\n"
+	#allResults.append(tmpResult)
+#	return sos
+
+
+#def get_stackoverflow_posts_over_time ():
+#	client = bigquery.Client()
+#
+#	get_SOPostsOverTime = """
+#		SELECT ph.*, pht.Type, tv.Title, pbv.Content FROM `sotorrent-org.2018_09_23.PostHistory` as ph, `sotorrent-org.2018_09_23.PostHistoryType` as pht,
+#		`sotorrent-org.2018_09_23.TitleVersion` as tv, `sotorrent-org.2018_09_23.PostVersion` as pv,
+#		`sotorrent-org.2018_09_23.PostBlockVersion` as pbv, `sotorrent-org.2018_09_23.PostBlockType` as pbt
+#		WHERE ph.PostHistoryTypeId = pht.Id and
+#		ph.PostId = tv.PostHistoryId and
+#		ph.PostId = pv.PostHistoryId and
+#		ph.PostId = pbv.PostHistoryId and
+#		pbv.PostBlockTypeId = pbt.Id;
+#	"""
+#
+#	query_job = client.query(get_SOPostsOverTime)
+#	results = query_job.result()
+#
+#	return results
+#
+#if __name__ == '__main__':
+#	allResults = get_stackoverflow_posts()
+#   	#get_stackoverflow_posts_over_time()
